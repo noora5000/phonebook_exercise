69cbdf23dbc8a973b28ff22471df2df770ca680a
noora5000 <109808100+noora5000@users.noreply.github.com>
2023-06-30 11:07:03 +0300

    Update index.js
    added database

--- index.js
+++ index.js
@@ -1,46 +1,31 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
+const Person = require('./models/person')
+
 morgan.token('body', (req) => {
   let body = JSON.stringify(req.body)
   return body
 })
-const cors = require('cors')
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' })
+}
 
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 app.use(cors())
 app.use(express.static('build'))
 
+
 let persons = [
-    {
-      id: 1,
-      name: "Arto Hellas",
-      number: "040-123456"
-    },
-    {
-      id: 2,
-      name: "Ada Lovelace",
-      number: "39-44-5323523"
-    },
-    {
-      id: 3,
-      name: "Dan Abramov",
-      number: "12-43-234345"
-    },
-    {
-      id: 4,
-      name: "Mary Poppendick",
-      number: "39-23-6423111"
-    },
 ]
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
-})
-
 app.get('/info', (req, res) => {
-    const len = persons.length
+    //const len = persons.length
+    // const len = req.body.content.length
     const timeStamp = new Date(Date.now())
     res.send(`<p>Phonebook has info for ${len} people.</p>
              <p>${timeStamp}</p>`)
@@ -48,68 +33,46 @@ app.get('/info', (req, res) => {
 })
 
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
+  Person.find({}).then(people => {
+    res.json(people)
+  })
 })
+
 
 app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const person = persons.find(person => {
-      return person.id === id
+  Person.findById(req.params.id)
+    .then(person => {
+      res.json(person)
     })
-    if (!person) {
-        res.status(404).end()
-    } else {
-        res.json(person)
-    }
 })
-
-
-const generateRandomId = () => {
-  const maxId = persons.length > 0
-    ? Math.max(...persons.map(n=>n.id))
-    : 0
-  const min = Math.ceil(maxId + 1)
-  const max = Math.floor(1000)
-  return Math.floor(Math.random() * (max - min) + min)
-}
 
 app.post('/api/persons', (req, res) => {
   const body = req.body
 
-  if(!body.name || !body.number) {
-    return req.status(400).json({ 
-        error: 'content missing.' 
-      })
-  }
-  if(persons.map(person=>person.name).includes(body.name)){
-    return res.status(400).json({
-        error: 'name must be unique.'
-      })
+  if (body.name === undefined || body.number === undefined) {
+    return res.status(400).json({ error: 'content missing' })
   }
 
-  const person = {
-    id: generateRandomId(),
+  const person = new Person({
     name: body.name,
     number: body.number,
-  }
+  })
 
-  persons = persons.concat(person)
-
-  res.json(person)
-
-  morgan.token('param', function(req, res, param) {
-    return req.params[param];
+  person.save().then(savedPerson => {
+    res.json(savedPerson)
   })
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(person => person.id !== id)
+  const id = Number(req.params.id)
+  persons = persons.filter(person => person.id !== id)
 
-    res.status(204).end()
+  res.status(204).end()
 })
 
-const PORT = process.env.PORT || 3001
+app.use(unknownEndpoint)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
